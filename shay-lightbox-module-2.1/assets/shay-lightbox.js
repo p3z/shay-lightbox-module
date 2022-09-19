9fefc76acdd9321d7082006abf11d828bb97bc7a
Shay <chenille.pottle@yahoo.co.uk>
2022-09-19 15:54:50 +0100

    Add start of caption functionality, and init_object functionality

--- shay-lightbox-module-2.1/assets/shay-lightbox.js
+++ shay-lightbox-module-2.1/assets/shay-lightbox.js
@@ -3,54 +3,83 @@
      A simple solution for handling lightboxes on both image tags and divs with a background image set
 */
 
+// config_obj hasnt been fully integrated yet
+const config_obj = {
+     captions: {
+          using_captions: false,
+          caption_location: "bottom" // will accept top or bottom
+          
+     },
+     custom_lightbox: false
+}
+
+
+
 // Attach the lightbox handler to divs with a class of "shay-lightbox-div". Background image is required
 function shay_lightbox_init_divs(){
-    jQuery('.shay-lightbox-div').click( (e) => {
-         let selected_image = e.target;
-         let image_url = getComputedStyle(selected_image).getPropertyValue("background-image");
-         
-         let actual_url = (image_url.match( /url\([^\)]+\)/gi ) || [""])[0].split(/[()'"]+/)[1];
-         
-         create_shay_lightboxes(actual_url);
-         
-    })
+     jQuery('.shay-lightbox-div').click( (e) => {
+          let selected_image = e.target;
+          let image_url = getComputedStyle(selected_image).getPropertyValue("background-image");          
+          let actual_url = (image_url.match( /url\([^\)]+\)/gi ) || [""])[0].split(/[()'"]+/)[1]; 
+          let caption = jQuery(e.target).attr("data-shay-lightbox-caption") ?? "";
+          
+          create_shay_lightboxes(actual_url, caption);
+          
+     })
 }
 
 // Attach the lightbox handler to image tags with a class of "shay-lightbox-img". src attribute is required
 function shay_lightbox_init_imgs(){
-    
+     
      jQuery('.shay-lightbox-img').click( (e) => {
-         let selected_image = e.target;
-         let image_url = jQuery(e.target).attr("src");
-         create_shay_lightboxes(image_url);          
-    })
+          let selected_image = e.target;
+          let image_url = jQuery(e.target).attr("src");
+          let caption = jQuery(e.target).attr("data-shay-lightbox-caption") ?? "";
+          create_shay_lightboxes(image_url, caption);          
+     });
+     
 }
 
 /* 1. Builds the lightbox element and prepends it to the document body
 * 2. Builds an image tag using the src url of the image clicked on. Appends this to the lightbox element
 * 3. Initialises a handler for removing the lightbox from the DOM
 */
-function create_shay_lightboxes(url){
-   let lightbox = '<div class="shay-lightbox-container"></div>';
+function create_shay_lightboxes(url, caption){
+    // Create a main lightbox container 
+    let lightbox = '<div class="shay-lightbox-container"></div>';
     jQuery('body').prepend(lightbox);     
+     
+    // Create any elements you want to add to that container (this will eventually be togglable so users can add their own elements/css)
+    let lightbox_image = `<img class="shay-zoomed-image"/>`;  
+    let lightbox_caption = `<div class="shay-lightbox-caption-area"></div>`;
+     
+    // Append the lightbox elements to the lightbox container
+    jQuery('.shay-lightbox-container').append(lightbox_image);
+     
+    // Set up the elements as needed
+    jQuery('.shay-zoomed-image').attr('src', url);
+     
+     if(caption){          
+          jQuery('.shay-lightbox-container').append(lightbox_caption);
+          jQuery('.shay-lightbox-caption-area').text(caption);
+     }
+     
     
-   let lightbox_image = `<img class="shay-zoomed-image"/>`;
-   jQuery('.shay-lightbox-container').append(lightbox_image);
-    
-   jQuery('.shay-zoomed-image').attr('src', url);
-   init_removal_handler();
-    
+     
+    // Enable the lightbox to be removed
+    init_removal_handler();
+     
 }
 
 
 /*
-* Destroy the lightbox when clicked
-This needs to happen AFTER the lightbox has been created (ie NOT on DOM load) -> otherwise no handler is attached
+* Destroys the lightbox when clicked
+ This needs to happen AFTER the lightbox has been created (ie NOT on DOM load) -> otherwise no handler is attached
 */
 function init_removal_handler(){
-     jQuery('.shay-lightbox-container').click( () => {
-         jQuery('.shay-lightbox-container').remove();
-     });     
+      jQuery('.shay-lightbox-container').click( () => {
+          jQuery('.shay-lightbox-container').remove();
+      });     
 }
 
 
